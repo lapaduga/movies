9a4218f04139a4d2189e1cc0f60a5758c31a33f4
Denis Kotov <deniskotov29042015@gmail.com>
2025-06-04 01:56:58 +0500

    data output

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,96 @@
+"use client";
+
+import { movies } from "../data/movies";
 import Image from "next/image";
+import { useSearchParams } from "next/navigation";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+  const searchParams = useSearchParams();
+  const genresList = movies.reduce((acc: string[], item) => {
+    acc = [...acc, ...item.genre];
+    return acc;
+  }, []);
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+  const yearsList = movies
+    .reduce((acc: number[], item) => {
+      if (!acc.includes(item.year)) {
+        acc.push(item.year);
+      }
+      return acc;
+    }, [])
+    .sort();
+
+  const genres = Array.from(new Set(genresList)).sort();
+  const params: { [key: string]: string } = {};
+  
+  searchParams.forEach((value, key) => {
+    params[key] = value;
+  });
+
+  console.log(params.genre.split(','));
+
+  return (
+    <div className="grid w-full max-w-[1200px] m-auto py-10 px-4">
+      <h1 className="text-4xl text-center">Movie Catalog</h1>
+      <form className="grid sm:grid-cols-2 gap-8 mt-8">
+        <fieldset>
+          <legend className="text-xl">Genres</legend>
+          <div className="flex flex-wrap gap-4 mt-3">
+            {genres.map((item, index) => (
+              <label
+                key={index}
+                className="flex items-center gap-2 cursor-pointer capitalize"
+              >
+                <input type="checkbox" name={item} value={item} />
+                {item}
+              </label>
+            ))}
+          </div>
+        </fieldset>
+        <fieldset>
+          <legend className="text-xl">Release Year</legend>
+          <select
+            name="years"
+            className="border mt-3 cursor-pointer rounded-lg px-4 py-2"
           >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+            <option
+              defaultValue="Choose release year"
+              hidden
+              className="text-(--background)"
+            >
+              Choose release year
+            </option>
+            {yearsList.map((year, index) => (
+              <option key={index} value={year} className="text-(--background)">
+                {year}
+              </option>
+            ))}
+          </select>
+        </fieldset>
+      </form>
+
+      <ul className="grid place-content-center sm:place-content-start sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 pt-10">
+        {movies.map((item) => (
+          <li key={item.id} className="rounded-xl overflow-hidden border">
+            <a href="#">
+              <Image
+                src="/dummy.jpg"
+                alt={item.title}
+                width={280}
+                height={173}
+                className="w-full object-fit aspect-video"
+              />
+              <div className="p-4">
+                <h2 className="text-xl">{item.title}</h2>
+                <div className="mt-2">Release date: {item.year}</div>
+                <div className="text-sm mt-2">
+                  Genre: {item.genre.join(", ")}
+                </div>
+              </div>
+            </a>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
